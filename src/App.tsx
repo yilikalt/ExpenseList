6161af7efe4f68920247242ac69c7b34217e7b73
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-11-21 15:16:27 +0300

    Update ExpenseForm.tsx and App.tsx
    Styling Remains

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import ExpenseList from "./Components/ExpenseList";
 import ExpenseFilter from "./Components/ExpenseFilter";
-import { ExpenseForm } from "./Components/ExpenseForm";
 import categories from "./Categories";
+import ExpenseForm from "./Components/ExpenseForm";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -16,11 +16,15 @@ function App() {
   const visibleExpenses = selectedCategory
     ? expenses.filter((e) => e.category === selectedCategory)
     : expenses;
-
+  // const onSubmit
   return (
     <div className="App">
       <div className="mb-5">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter
--- src/Components/ExpenseForm.tsx
+++ src/Components/ExpenseForm.tsx
@@ -2,7 +2,6 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import categories from "../Categories";
-
 const schema = z.object({
   description: z
     .string()
@@ -17,18 +16,23 @@ const schema = z.object({
   }),
 });
 type ExpenseFormData = z.infer<typeof schema>;
-
-export const ExpenseForm = () => {
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+const ExpenseForm = ({ onSubmit }: Props) => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    reset,
+    formState: { errors, isValid },
   } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
-  //   const onSubmit = (data: FieldValues) => {
-  //     console.log(data);
-  //   };
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form
+      onSubmit={handleSubmit((data) => {
+        onSubmit(data);
+        reset();
+      })}
+    >
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
@@ -48,7 +52,7 @@ export const ExpenseForm = () => {
           Amount
         </label>
         <input
-          {...register("amount")}
+          {...register("amount", { valueAsNumber: true })}
           id="amount"
           type="number"
           className="form-control"
@@ -62,7 +66,9 @@ export const ExpenseForm = () => {
           Category
         </label>
         <select {...register("category")} id="category" className="form-select">
-          <option value=""></option>
+          <option value="" disabled>
+            Select a category
+          </option>
           {categories.map((category) => (
             <option key={category} value={category}>
               {category}
@@ -73,9 +79,10 @@ export const ExpenseForm = () => {
           <p className="text-danger">{errors.category.message}</p>
         )}
       </div>
-      <button type="submit" className="btn btn-primary">
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
         Submit
       </button>
     </form>
   );
 };
+export default ExpenseForm;
