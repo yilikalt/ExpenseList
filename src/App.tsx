6e7e34de31ce980b0e1d772ecb9f30eff629f59e
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-11-21 13:18:06 +0300

    updating expenselist.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import "./App.css";
+import ExpenseList from "./Components/ExpenseList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 700, category: "utility" },
+    { id: 2, description: "vvv", amount: 100, category: "activities" },
+    { id: 3, description: "bbb", amount: 200, category: "utility" },
+    { id: 4, description: "nnn", amount: 900, category: "utility" },
+  ]);
 
-  return <div className="App">hello world</div>;
+  return (
+    <div className="App">
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      />
+    </div>
+  );
 }
 
 export default App;
